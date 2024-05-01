e7976c9bda5695486dfa8fc38ba4e176d8aabd48
Diego <131261339+DiegoNoxe@users.noreply.github.com>
2024-05-01 13:56:53 -0300

    a

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('history').addEventListener('click', function() {
+    alert('Link leva para History!');
+});
+
+document.getElementById('channel').addEventListener('click', function() {
+    alert('Link leva para Channel!');
+});
+
+document.getElementById('instagram').addEventListener('click', function() {
+    alert('O link leva para o Instagram!');
+});
